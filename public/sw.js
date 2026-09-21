1ca45ae1fd1d06ae082f0dbeb9b8755367811db4
adedgestudio-dotcom <zorivoworks@gmail.com>
2026-09-21 16:01:58 +0530

    Force service worker refresh for phone push fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "pakki-baat-v2";
+const VERSION = "pakki-baat-v3";
 const SHELL_CACHE = VERSION + "-shell";
 const RUNTIME_CACHE = VERSION + "-runtime";
 const CORE = ["/manifest.json", "/icon.svg", "/apple-icon.svg"];
